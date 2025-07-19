65d135022127253fad144229a23c53791d92f687
Anandhakrishnan <anandhakrishnans2025@gmail.com>
2025-07-19 13:21:38 +0530

    phpv1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 echo '
 <div style="text-align:center; padding-top:100px; font-family:Arial;">
-    <h1 style="color:#0078d7;">Welcome to <span style="color:#ff6600;">Tamil Azure DevOps</span> Study</h1>
+    <h1 style="color:#0078d7;">Welcome to <span style="color:#ff6600;">Tamil Azure DevOps v1</span> Study</h1>
     <p style="color:#555;">Happy Learning!</p>
 </div>
 ';
